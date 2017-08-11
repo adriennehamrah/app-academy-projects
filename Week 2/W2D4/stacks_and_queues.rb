99967c4d29104d238bb1221b22a97eb6d16ca736
Adrienne Lin <lin.adrienne@gmail.com>
2017-08-11 12:51:40 -0700

    -Add min max stack queue

--- Week 2/W2D4/stacks_and_queues.rb
+++ Week 2/W2D4/stacks_and_queues.rb
@@ -36,8 +36,7 @@ class MyStack
   end
 
   def push(el)
-    @store.push({el => @max})
-    @max = el if el > @max
+    @store.push(el)
   end
 
   def peek
@@ -69,6 +68,96 @@ class StackQueue
       @out_stack.push(@in_stack.pop)
     end
     @out_stack.pop
+  end
+
+  def size
+    @in_stack.length + @out_stack.length
+  end
+
+  def empty?
+    @in_stack.empty? && @out_stack.empty?
+  end
+end
+
+class MinMaxStack
+  def initialize
+    @store = Mystack.new
+  end
+
+  def push(el)
+    @store.push(
+      max: new_max(val),
+      min: new_min(val),
+      value: val
+    )
+  end
+
+  def pop
+    @store.pop[:value] unless empty?
+  end
+
+  def max
+    @store.peek[:max] unless empty?
+  end
+
+  def min
+    @store.peek[:min] unless empty?
+  end
+
+  def peek
+    max = @max
+    @store.peek[:value] unless empty?
+  end
+
+  def size
+    @store.length
+  end
+
+  def empty?
+    @store.empty?
+  end
+
+  private
+
+  def new_max(val)
+    empty? ? val : [max, val].max
+  end
+
+  def new_min(val)
+    empty? ? val : [min, val].min
+  end
+end
+
+class MaxMinStackQueue
+  def initialize
+    @in_stack = MinMaxStack.new
+    @out_stack = MinMaxStack.new
+  end
+
+  def enqueue(el)
+    @in_stack.push(el)
+  end
+
+  def dequeue
+    return @out_stack.pop unless @out_stack.empty?
+    until @in_stack.empty?
+      @out_stack.push(@in_stack.pop)
+    end
+    @out_stack.pop
+  end
+
+  def max
+    maxes = []
+    maxes << @in_stack.max unless @in_stack.empty?
+    maxes << @out_stack.max unless @out_stack.empty?
+    maxes.max
+  end
+
+  def min
+    mins = []
+    mins << @in_stack.min unless @in_stack.empty?
+    mins << @out_stack.min unless @out_stack.empty?
+    mins.min
   end
 
   def size
